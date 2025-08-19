7bf269ee76e634e2e99783c72db3686217862827
Glenn Renfro <glenn.renfro@broadcom.com>
2025-08-19 08:11:32 -0400

    Revert to Boot 3.5
    Revert jackson3 back to jackson 2
    Add tests

--- dashboard/src/main/java/io/spring/dashboard/statistics/DashboardConfiguration.java
+++ dashboard/src/main/java/io/spring/dashboard/statistics/DashboardConfiguration.java
@@ -3,7 +3,7 @@ package io.spring.dashboard.statistics;
 
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,7 +13,7 @@ public class DashboardConfiguration {
 
 	@Bean
 	public MessageConverter jsonMessageConverter() {
-		return new JacksonJsonMessageConverter();
+		return new Jackson2JsonMessageConverter();
 	}
 
 	@Bean
